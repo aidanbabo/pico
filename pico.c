18a1cdfa630e5820ae033c42316bef65b5c73986
aidanbabo <aidan.babo@gmail.com>
2021-02-06 03:21:15 -0500

    Search

--- pico.c
+++ pico.c
@@ -73,7 +73,7 @@ struct EditorConfig E;
 
 void editor_set_status_message(const char *fmt, ...);
 void editor_refresh_screen();
-char* editor_prompt(char* prompt);
+char* editor_prompt(char* prompt, void (*callback)(char*, int));
 
 /*** terminal ***/
 
@@ -249,6 +249,22 @@ int editor_row_cx_to_rx(Row *row, int cx) {
         rx++;
     }
     return rx;
+}
+
+int editor_row_rx_to_cx(Row* row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++) {
+        if (row->chars[cx] == '\t') {
+            cur_rx += (PICO_TAB_STOP - 1) - (cur_rx % PICO_TAB_STOP);
+        }
+        cur_rx++;
+
+        if (cur_rx > rx)
+            return cx;
+    }
+
+    return cx;
 }
 
 void editor_update_row(Row* row) {
@@ -440,7 +456,7 @@ void editor_open(char *filename) {
 
 void editor_save() {
     if (E.filename == NULL) {
-        E.filename = editor_prompt("Save as: %s");
+        E.filename = editor_prompt("Save as: %s", NULL);
         if (E.filename == NULL) {
             editor_set_status_message("Save aborted");
             return;
@@ -473,6 +489,67 @@ void editor_save() {
     editor_set_status_message("Can't save! I/O error: %s", strerror(errno));
 }
 
+/*** find ***/
+
+void editor_find_callback(char* query, int key) {
+    static int last_match = -1;
+    static int direction = 1;
+
+    if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == -1)
+        direction = 1;
+    int current = last_match;
+
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        // start from last match and wrap around
+        current += direction;
+        if (current == -1)
+            current = E.numrows - 1;
+        else if (current == E.numrows) 
+            current = 0;
+
+        Row* row = &E.row[current];
+        char* match = strstr(row->render, query);
+        if (match) {
+            last_match = current;
+            E.cy = current;
+            E.cx = editor_row_rx_to_cx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+}
+
+void editor_find() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+    char* query = editor_prompt("Search: %s (Use ESC/Arrows/Enter)", editor_find_callback);
+
+    if (query) {
+        free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+    }
+}
+
 /*** append buffer ***/
 
 // Avoid many small calls to write
@@ -500,7 +577,8 @@ void abFree(struct abuf *ab) {
 
 /*** input ***/
 
-char* editor_prompt(char* prompt) {
+// callback function called after every keypress
+char* editor_prompt(char* prompt, void (*callback)(char*, int)) {
     size_t bufsize = 128;
     char* buf = malloc(bufsize);
 
@@ -518,11 +596,15 @@ char* editor_prompt(char* prompt) {
         } else if (c == '\x1b') {
             // escape
             editor_set_status_message("");
+            if (callback)
+                callback(buf, c);
             free(buf);
             return NULL;
         } else if (c == '\r') {
             if (buflen != 0) {
                 editor_set_status_message("");
+                if (callback)
+                    callback(buf, c);
                 return buf;
             }
         } else if (!iscntrl(c) && c < 128) {
@@ -533,6 +615,9 @@ char* editor_prompt(char* prompt) {
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+
+        if (callback)
+            callback(buf, c);
     }
 }
 
@@ -612,6 +697,10 @@ void editor_process_keypress() {
         case END_KEY:
             if (E.cy < E.numrows)
                 E.cx = E.row[E.cy].size;
+            break;
+
+        case CTRL_KEY('f'):
+            editor_find();
             break;
 
         case BACKSPACE:
@@ -839,7 +928,7 @@ int main(int arc, char *argv[]) {
         editor_open(argv[1]);
     }
 
-    editor_set_status_message("HELP: Ctrl-S to save | Ctrl-Q to quit");
+    editor_set_status_message("HELP: Ctrl-S to save | Ctrl-Q to quit | Ctrl-F to find");
 
     for (;;) {
         editor_refresh_screen();
